ea28c13291175d926a66e8be4aaf54f20b6d10cb
DoksaVPC <55451950+DoksaVPC@users.noreply.github.com>
2022-07-18 16:14:15 +0200

    updated video file, changed router to hash

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Lottie from "react-lottie-player";
 import introAnimation from "./assets/intro_animation.json";
 import ScrollToTop from "./components/ScrollToTop";
 import { useState } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Footer from "./components/shared/Footer";
 
 function App() {
